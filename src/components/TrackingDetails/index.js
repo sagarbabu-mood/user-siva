27bc5763f1ca5ba5b352b4b2565a7b7633cf49b6
root <SivaNarayana150>
2024-02-22 13:48:59 +0000

    cant fetch data from backend

---
+++ src/components/TrackingDetails/index.js
@@ -0,0 +1,14 @@
+import Component from "React"
+
+
+class TrackingDetails extends Component{
+    render(){
+        return (
+         
+        <h1>Hello</h1>
+
+        )
+    }
+}
+
+export default TrackingDetails
